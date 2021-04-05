016fcb31c716923667b58221aba90367aeff73d1
Sonam <nimaytenzin@gmail.com>
2021-04-05 15:49:26 +0600

    asdsdssd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,7 @@ export const environment = {
   production: true,
   // API_URL: 'http://43.230.208.80:8080'
   // API_URL: 'https://zhichar.myddns.rocks/api'
-  API_URL : 'http://localhost:3000/api'
+  // API_URL : 'http://localhost:3000/api'
+  API_URL: 'https://unepassapi.herokuapp.com/api'
+
 };
